bdb884b4b2364c7c4816550cbe5546bb906fdda1
Sean Cahill <seancahill25@gmail.com>
2018-02-01 05:32:06 +0000

    added tabs and extra car

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
 /*global angular */
 (function() {
     var app = angular.module('store', []);
-    
     app.controller('StoreController', function() {
         this.products = cars;
     });
-    
-    var cars = [
-        {
+    var cars = [{
         type: 'Sedan',
         description: '...',
         price: 30.00,
         canPurchase: true,
         soldOut: false,
-        images: [
-            {
-                full: "images/sedan.png",
-                thumb: "",
-            },
-            {
-                full: "images/suv.png",
-                thumb: "",
-            },
-            {
-                full: "",
-                thumb: "",
-            }]
-    },
-    {type: 'SUV',
+        images: [{
+            full: "images/sedan.png",
+        }]
+    }, {
+        type: 'SUV',
         description: '...',
         price: 40.00,
         canPurchase: true,
         soldOut: false,
-    }
-];    
+        images: [{
+            full: "images/suv.png",
+        }]
+    }, {
+        type: 'Coupe',
+        description: '...',
+        price: 60.00,
+        canPurchase: true,
+        soldOut: false,
+        images: [{
+            full: "images/coupe.png",
+        }]
+    }];
 })();
